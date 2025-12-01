8bc8ff246bd2a27e8b2f502ea74a1dafaa10ef22
jelinekp <jelinekp6@gmail.com>
2025-12-01 20:32:47 +0100

    Input Test

--- src/Day01.kt
+++ src/Day01.kt
@@ -11,8 +11,8 @@ fun main() {
     check(part1(listOf("test_input")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day01")
+    check(part1(testInput) == 4671)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
