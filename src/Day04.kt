5e1b8cdf5f7a117821dd702e3d07888cb9fad61e
jelinekp <jelinekp6@gmail.com>
2025-12-05 18:07:06 +0100

    Refactor Day04.kt

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,116 +1,63 @@
-import java.lang.Exception
+data class Point(val x: Int, val y: Int) {
+    fun neighbors(): List<Point> =
+        (-1..1).flatMap { dx ->
+            (-1..1).map { dy -> Point(x + dx, y + dy) }
+        }.filter { it != this } // Exclude (0,0) - ourselves
+}
 
-class RollsWallSolver(
-    val input: List<String>
-) {
-    private var rollsWall: MutableList<MutableList<Int>> = mutableListOf()
-    private var height: Int = 0
-    private var width: Int = 0
+class RollsWallSolver(input: List<String>) {
 
-    fun readInput() {
-        rollsWall = mutableListOf()
+    // We store only @ in the set
+    private val initialRolls: Set<Point>
 
-        input.forEach { line ->
-
-            val lineValues = MutableList(line.length) { 0 }
-
-            line.toCharArray().forEachIndexed { index, char ->
-                if (char == '@')
-                    lineValues[index] = 1
-            }
-
-            rollsWall.add(lineValues)
-        }
-
-        height = rollsWall.size
-        width = rollsWall.first().size
-    }
-
-    fun printRollsWall() =
-        rollsWall.forEach { row ->
-            println(row)
-        }
-
-    fun getNumberOfAdjacentRolls(lineIndex: Int, columnIndex: Int): Int {
-        var rolls = -1 // we don't want to count ourselves, so we start from -1
-
-        for (i in lineIndex - 1 .. lineIndex + 1) {
-            for (j in columnIndex - 1 .. columnIndex + 1) {
-                try {
-                    if (rollsWall[i][j] == 1)
-                        ++rolls
-                } catch (_: Exception) { // index out of bound exception
-                    continue
+    init {
+        val rolls = mutableSetOf<Point>()
+        input.forEachIndexed { row, line ->
+            line.forEachIndexed { col, char ->
+                if (char == '@') {
+                    rolls.add(Point(row, col))
                 }
             }
         }
-
-        return rolls
+        initialRolls = rolls
     }
 
-    fun findNumberOfLooseRolls(): Int {
-        var sumOfLooseRolls = 0
-
-        for (i in 0..<height)
-            for (j in 0..<width)
-                if (rollsWall[i][j] == 1)
-                    if (getNumberOfAdjacentRolls(i, j) < 4 )
-                        sumOfLooseRolls +=  1
-
-        return sumOfLooseRolls
+    private fun isLoose(point: Point, currentRolls: Set<Point>): Boolean {
+        // Count how many neighbors are there
+        val neighborCount = point.neighbors().count { it in currentRolls }
+        return neighborCount < 4
     }
 
-    fun findNumberOfLooseRollsForPartTwo(): Int {
-        var sumOfLooseRolls = 0
-
-        for (i in 0..<height)
-            for (j in 0..<width)
-                if (rollsWall[i][j] == 1)
-                    if (getNumberOfAdjacentRolls(i, j) < 4 ) {
-                        sumOfLooseRolls +=  1
-                        rollsWall[i][j] = 0
-                    }
-
-        return sumOfLooseRolls
+    fun solvePart1(): Int {
+        return initialRolls.count { isLoose(it, initialRolls) }
     }
 
-    fun findNumberOfLooseRollsRepeatedly(): Int {
-        var allRoundsSum = 0
+    fun solvePart2(): Int {
+        // We make a mutable copy to update every iteration
+        val currentRolls = initialRolls.toMutableSet()
+        var totalRemoved = 0
 
-        do {
-            val roundSum = findNumberOfLooseRollsForPartTwo()
-            allRoundsSum += roundSum
-        } while (roundSum > 0)
+        while (true) {
+            // 1. Find all rolls that need to be removed in this round
+            val toRemove = currentRolls.filter { isLoose(it, currentRolls) }.toSet()
 
-        return allRoundsSum
+            // 2. If nothing to remove, we are done
+            if (toRemove.isEmpty()) break
+
+            // 3. Update state
+            currentRolls.removeAll(toRemove)
+            totalRemoved += toRemove.size
+        }
+
+        return totalRemoved
     }
 }
 
 fun main() {
-    fun part1(input: List<String>): Int {
-
-        val solver = RollsWallSolver(input)
-        solver.readInput()
-        // solver.printRollsWall()
-        return solver.findNumberOfLooseRolls()
-    }
-
-    fun part2(input: List<String>): Int {
-
-        val solver = RollsWallSolver(input)
-        solver.readInput()
-        // solver.printRollsWall()
-        return solver.findNumberOfLooseRollsRepeatedly()
-    }
-
-    part1(readInput("test04")).println()
-    part2(readInput("test04")).println()
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(readInput("test04")) == 13)
-    check(part2(readInput("test04")) == 43)
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day04")
-    part1(input).println()
-    part2(input).println()
+
+    val solver = RollsWallSolver(input)
+
+    println("Part 1: ${solver.solvePart1()}")
+    println("Part 2: ${solver.solvePart2()}")
 }
