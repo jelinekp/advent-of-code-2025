2c2e208fa4d9a29150a3669792862f0d1d60a31d
jelinekp <jelinekp6@gmail.com>
2025-12-07 21:16:11 +0100

    Day 07 second part completed

--- src/Day07_second_try.kt
+++ src/Day07_second_try.kt
@@ -2,16 +2,24 @@ class TachyonBeamSolver2(
     val input: List<String>
 ) {
 
-    private val maxLineIdx = input.lastIndex
+    private val numberOfLines = input.size
+    private val lineWidth = input.first().lastIndex
 
-    fun startPropagation(): Int {
+    private val numberHexFormat = HexFormat {
+        upperCase = true
+        number {
+            removeLeadingZeros = true
+        }
+    }
+
+    fun getNumberOfSplits(): Int {
         val positionOfS = input.first().indexOfFirst { it == 'S' }
 
-        val lineIndexList = List(maxLineIdx + 1) { mutableSetOf<Int>() }
+        val lineIndexList = List(numberOfLines) { mutableSetOf<Int>() }
         lineIndexList[1].add(positionOfS)
 
         var splits = 0
-        for (i in 2..maxLineIdx) {
+        for (i in 2..<numberOfLines) {
             lineIndexList[i - 1].forEach { index ->
                 if (input[i][index] == '^') {
                     splits++
@@ -26,26 +34,72 @@ class TachyonBeamSolver2(
         return splits
     }
 
+    fun getNumberOfWays(): Long {
+        val positionOfS = input.first().indexOfFirst { it == 'S' }
+
+        val lineIndexList = MutableList(numberOfLines) { mutableMapOf<Int, Long>() }
+        lineIndexList[1][positionOfS] = 1
+
+        // we can skip lines without meaningful content with step(2)
+        for (i in 3..numberOfLines step(2)) {
+
+            val previousLine = lineIndexList[i - 2]
+
+            previousLine.forEach { (index, value) ->
+
+                if (input[i - 1][index] == '^') {
+                    val newLeftVal = lineIndexList[i].getOrDefault(index - 1, 0)
+                    val newRightVal = lineIndexList[i].getOrDefault(index + 1, 0)
+
+                    lineIndexList[i][index - 1] = newLeftVal + value
+
+                    lineIndexList[i][index + 1] = newRightVal + value
+
+                    lineIndexList[i].remove(index)
+                } else {
+                    val currentVal = lineIndexList[i].getOrDefault(index, 0)
+                    lineIndexList[i][index] = value + currentVal
+                }
+            }
+
+            // to reconstruct the tree visually :)
+            printRowFromLineIndexList(previousLine)
+            println(input[i - 1])
+        }
+
+        printRowFromLineIndexList(lineIndexList.last())
+
+        val tachyonWays = lineIndexList.last().values.sum()
+        println("Tachyon ways: $tachyonWays")
+        return tachyonWays
+    }
+
+    private fun printRowFromLineIndexList(lineIndexMap: Map<Int, Long>) {
+        for (i in 0..lineWidth) {
+            if (lineIndexMap.containsKey(i)) {
+                print(lineIndexMap[i]?.toHexString(numberHexFormat))
+            } else {
+                print('.')
+            }
+        }
+        println()
+    }
 }
 
 fun main() {
     fun part1(input: List<String>): Int {
         val solver = TachyonBeamSolver2(input)
-        return solver.startPropagation()
+        return solver.getNumberOfSplits()
     }
 
-    fun part2(input: List<String>): Int {
+    fun part2(input: List<String>): Long {
         val solver = TachyonBeamSolver2(input)
-        return 0 // solver.startPropagationPartTwo()
+        return solver.getNumberOfWays()
     }
 
     println("Part 1:")
     part1(readInput("test07")).println()
-
-    // Test if implementation meets criteria from the description:
     check(part1(readInput("test07")) == 21)
-
-    // Read the input from the `src/Day01.txt` file.
 
     val input = readInput("Day07")
     part1(input).println()
@@ -53,6 +107,6 @@ fun main() {
     println("Part 2:")
 
     part2(readInput("test07")).println()
-    check(part2(readInput("test07")) == 40)
+    check(part2(readInput("test07")) == 40L)
     part2(input).println()
 }
